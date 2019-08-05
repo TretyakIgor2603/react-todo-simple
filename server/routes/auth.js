921d7c8046f466e5f270d65fd67d60e5c8940d03
developer <dev@example.com>
2019-08-05 10:01:26 +0300

    after merge with home

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,13 @@
 import * as controllers from "../controllers/auth";
-import Router from "express-promise-router"
-const router = Router()
+import Router from "express-promise-router";
+const router = Router();
+import withAuth from "../middleware/auth";
 
-router.post('/user-exist', controllers.userExist)
-router.post('/login', controllers.validate('login'), controllers.login)
-router.post('/register', controllers.register)
+router.post("/user-exist", controllers.userExist);
+router.post("/login", controllers.validate("login"), controllers.login);
+router.post("/register", controllers.register);
+router.get("/check-token", withAuth, function(req, res) {
+  res.sendStatus(200);
+});
 
 export default router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,9 @@
 import * as controllers from "../controllers/user";
-import auth from "../middleware/auth";
+import withAuth from "../middleware/auth";
 import Router from "express-promise-router";
 const router = Router();
 
-router.get("/", auth, controllers.getUsers);
+router.get("/", withAuth, controllers.getUsers);
+
 
 export default router;
