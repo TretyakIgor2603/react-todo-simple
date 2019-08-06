fbe3f8ddb265cfb2b480b97e2e3788fa2f43d1c6
h1tan <axe.dota@mail.ru>
2019-08-06 22:43:30 +0300

    [fix]

---
+++ client/src/store/user/user-actions.js
@@ -0,0 +1,22 @@
+import { getToken } from "../auth/auth-actions";
+
+export const FETCH_USER_DATA = "FETCH_USER_DATA";
+export const fetchUserData = () => {
+  const token = getToken();
+  if (token) {
+    return {
+      type: FETCH_USER_DATA,
+      request: {
+        url:  `/user/`,
+        method: "get",
+        headers: {
+          Authorization: token
+        }
+      },
+      meta: { token }
+    };
+  } else {
+    return { type: FETCH_USER_DATA };
+  }
+};
+asd
